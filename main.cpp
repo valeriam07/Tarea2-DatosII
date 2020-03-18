d84d60134d11159cac3d506c9f6ec76102261ae2
Valeria <valeriamorales40@gmail.com>
2020-03-18 15:21:10 -0600

    Created class Pug, Pomerania and PomeraniaAdapter. Void sleep changed to pet.

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@ using namespace std;
 class Dog{                      /* abstract class */
     virtual void bark() = 0;
     virtual void eat() = 0;
-    virtual void sleep() = 0;
+    virtual void pet() = 0;
 };
 
 class Chihuahua: public Dog {
 public:
     Chihuahua(){
-        cout<< "Chihuahua" <<endl;
+        cout<< "You have adopted a Chihuahua!!!" <<endl;
     }
     void bark() override{
         cout << "your Chihuahua is barking" << endl;
@@ -20,12 +20,60 @@ public:
         cout << "your Chihuahua is eating" << endl;
     }
 
-    void sleep() override {
-        cout << "your Chihuahua is sleeping" << endl;
+    void pet() override {
+        cout << "you are petting your Chihuahua" << endl;
     }
 
 };
+
+class Pug: public Dog {
+public:
+    Pug(){
+        cout<< "You have adopted a Pug!!!" <<endl;
+    }
+    void bark() override{
+        cout << "your Pug is barking" << endl;
+    }
+
+    void eat() override {
+        cout << "your Pug is eating" << endl;
+    }
+
+    void pet() override {
+        cout << "you are petting your Pug" << endl;
+    }
+
+};
+
+class Pomerania{
+public:
+    void brushHair(){
+        cout<< "you are brushing your Pomerania's hair"<<endl;
+    }
+};
+
+
+class PomeraniaAdapter: public Dog{
+public:
+    PomeraniaAdapter(){
+        cout<< "Adapting Pomerania..." <<endl;
+    }
+    void bark() override{
+        cout << "your Pomerania is barking" << endl;
+    }
+
+    void eat() override {
+        cout << "your Pomerania is eating" << endl;
+    }
+
+    void pet() override {
+        cout << "you are petting your Pomerania" << endl;
+        Pomerania p;
+        p.brushHair();
+    }
+};
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
